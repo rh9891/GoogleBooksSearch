84b8c9d8423bd80aa4029a795a751a0dd1582301
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-15 18:32:14 -0400

    Imported the search component to the App file. Need to adjust the CSS for the file as the search box isn't positioned as expected.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import NavBar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
 import Wrapper from "./components/Wrapper";
+import Search from "./components/Search";
 
 function App() {
   return (
     <Wrapper>
     <NavBar />
     <Jumbotron />
+    <Search />
     </Wrapper>
   );
 }
