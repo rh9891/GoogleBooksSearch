0e75e75b8fb9176c84a0d9c642e5377a3476dbdf
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 07:20:09 -0400

    Made connection to Mongoose database for the seeds file.

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,3 +1,9 @@
 // Pulls in the required dependencies.
 const mongoose = require("mongoose");
 const db = require("../models");
+
+// This file empties the Books collection and inserts the books below.
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+);
