f67975bfd0de239d8d802a6e69e8cd49dee826da
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 07:09:02 -0400

    Completed and exported the router file for the book routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,8 @@
 // Pulls in the required dependencies.
 const router = require("express").Router();
 const bookRoutes = require("./books");
+
+// Book routes.
+router.use("/books", bookRoutes);
+
+module.exports = router;
