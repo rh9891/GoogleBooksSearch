c6c232f6462d5f199062c0e045bbc7cd52224e67
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 09:46:45 -0400

    Finished the HTML/CSS layout of the results divfor the searches, so that they can be properly appended onto the screen.

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -4,12 +4,31 @@ import "./style.css";
 function Results() {
   return (
 <>
-<article class="message is-primary">
-  <div class="message-header" id="resultsHeaderText">
+<article className="message is-primary">
+  <div className="message-header" id="resultsHeaderText">
     <p>Results</p>
-    <button class="delete" id="bookSearchResultsDeleteButton" aria-label="delete"></button>
+    <button className="delete" id="bookSearchResultsDeleteButton" aria-label="delete"></button>
   </div>
-  <div class="message-body" id="bookSearchResultsDiv">
+
+  <div className="message-body" id="bookSearchResultsDiv">
+  <div className="columns is-mobile is-desktop">
+  <div className="column is-1"></div>
+  <div className="column is-3">
+  <figure className="image is-128x128">
+  <img src="https://bulma.io/images/placeholders/128x128.png" />
+  </figure>
+  </div>
+  <div className="column">
+  <h5 className="bookTitle"></h5>
+  <p className="bookDescriptionText"></p>
+  </div>
+  <div className="column is-2">
+  <div className="buttons">
+  <button className="button is-primary" id="viewButton">View</button>
+  <button className="button is-primary" id="saveButton">Save</button>
+  </div>
+  </div>
+  </div>
   </div>
 </article>
 </>
