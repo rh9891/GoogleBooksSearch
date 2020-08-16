2e90f8bc66ed060967eb9c6e885611198cea6fcb
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 03:07:16 -0400

    Completed the code for the server file. Added the routes, server port, and static files.

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 // Pulls in the required dependencies.
 const express = require("express");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const app = express();
+
+// Establishes a PORT to be used when running the application locally or for deployment.
+const PORT = process.env.PORT || 3001;
+
+// Middleware.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connection to the MongoDB.
+mongoose.connect(process.env.MONGOD_URI || "mongod://localhost/googlebookssearch");
+
+// Static files that will be used upon deployment to Heroku.
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Routes
+app.use(routes)
+
+// Set up the server.
+app.listen(PORT, function() {
+    console.log(`Server is now listening on ${PORT}...`);
+});
