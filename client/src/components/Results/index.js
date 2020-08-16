51a9de6e3e47f799a2bf011df38d2ea02b0dd25f
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-15 20:14:48 -0400

    Created the card layout for the Results component.

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -3,7 +3,20 @@ import "./style.css";
 
 function Results() {
   return (
+<>
 <br />
+<article class="message is-primary">
+  <div class="message-header">
+    <p>Results</p>
+    <button class="delete" id="deleteButton" aria-label="delete"></button>
+  </div>
+  <div class="message-body">
+    
+    
+
+  </div>
+</article>
+</>
   );
 }
 
