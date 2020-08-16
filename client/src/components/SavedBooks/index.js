0dd399c105316f8aeda73623ba92f96d4b73e0f7
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 18:16:19 -0400

    Added the componentDidMount function. Coded the savedBooks div, so that once I have made the other connections, the information from the axios call should be able to be rendered onto the page.

--- client/src/components/SavedBooks/index.js
+++ client/src/components/SavedBooks/index.js
@@ -4,16 +4,27 @@ import API from "../../utils/API";
 import Results from "../Results";
 
 class SavedBooks extends Component() {
+  state = {
+    savedBooks = [],
+  }
+
+  componentDidMount() {
+    API.savedBooks()
+      .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+      .catch(err => console.error(err));
+  }
+
   render() {
   return (
       <>
      <br />
-     <article class="message is-primary">
-  <div class="message-header" id="savedBooksHeaderText">
+     <article className="message is-primary">
+  <div className="message-header" id="savedBooksHeaderText">
     <p>Saved Books</p>
-    <button class="delete" id="savedBooksDeleteButton" aria-label="delete"></button>
+    <button className="delete" id="savedBooksDeleteButton" aria-label="delete"></button>
   </div>
-  <div class="message-body" id="savedBooksResultsDiv">
+  <div className="message-body" id="savedBooksResultsDiv">
+    <Results books={this.state.savedBooks} />
   </div>
 </article>
 </>
