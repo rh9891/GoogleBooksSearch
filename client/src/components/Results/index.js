1cc091bbc9657a308774b6aaa15bbe76e83a4baa
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 17:41:31 -0400

    Coded the results div, so that once I have made the other connections, the information from the axios call should be able to be rendered onto the page.

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,63 +1,111 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./style.css";
-import API from "../../utils/API"
+import API from "../../utils/API";
 
 class Results extends Component() {
   state = {
     savedBooks: [],
-  }
+  };
 
   componentDidMount() {
     API.savedBooks()
-      .then(savedBooks => this.setState({ savedBooks: savedBooks }))
-      .catch(err => console.error(err));
+      .then((savedBooks) => this.setState({ savedBooks: savedBooks }))
+      .catch((err) => console.error(err));
   }
 
-  handleSave = book => {
-    if (this.state.savedBooks.map(book => book._id).includes(book._id)) {
+  handleSave = (book) => {
+    if (this.state.savedBooks.map((book) => book._id).includes(book._id)) {
       API.deleteBook(book._id)
-        .then(deletedBook => this.setState({ savedBooks: this.state.savedBooks.filter(book => book._id !== deleteBook._id) }))
-        .catch(err => console.error(err));
+        .then((deletedBook) =>
+          this.setState({
+            savedBooks: this.state.savedBooks.filter(
+              (book) => book._id !== deleteBook._id
+            ),
+          })
+        )
+        .catch((err) => console.error(err));
     } else {
       API.saveBook(book)
-        .then(savedBook => this.setState({ savedBooks: this.state.savedBooks.concat([savedBook])}))
-        .catch(err => console.error(err));
+        .then((savedBook) =>
+          this.setState({
+            savedBooks: this.state.savedBooks.concat([savedBook]),
+          })
+        )
+        .catch((err) => console.error(err));
     }
-  }
+  };
 
   render() {
-  return (
-<>
-<article className="message is-primary">
-  <div className="message-header" id="resultsHeaderText">
-    <p>Results</p>
-    <button className="delete" id="bookSearchResultsDeleteButton" aria-label="delete"></button>
-  </div>
+    return (
+      <article className="message is-primary">
+        <div className="message-header" id="resultsHeaderText">
+          <p>Results</p>
+          <button
+            className="delete"
+            id="bookSearchResultsDeleteButton"
+            aria-label="delete"
+          ></button>
+        </div>
 
-  <div className="message-body" id="bookSearchResultsDiv">
-  <div className="columns is-mobile is-desktop">
-  <div className="column is-1"></div>
-  <div className="column is-3">
-  <figure className="image is-128x128">
-  <img src="https://bulma.io/images/placeholders/128x128.png" />
-  </figure>
-  </div>
-  <div className="column">
-  <h5 className="bookTitle"></h5>
-  <p className="bookDescriptionText"></p>
-  </div>
-  <div className="column is-2">
-  <div className="buttons">
-  <button className="button is-primary" id="viewButton">View</button>
-  <button className="button is-primary" id="saveButton">Save</button>
-  </div>
-  </div>
-  </div>
-  </div>
-</article>
-</>
-  );
+        <div>
+          {!this.props.books.length ? (
+            <h2 className="noResultsDisplay">
+              No information available for this book.
+            </h2>
+          ) : (
+            <div>
+              {this.props.books.map((result) => (
+                <div
+                  className="message-body"
+                  id="bookSearchResultsDiv"
+                  key={result._id}
+                >
+                  <div className="columns is-mobile is-desktop">
+                    <div className="column is-1"></div>
+                    <div className="column is-3">
+                      <figure className="image is-128x128">
+                        <img alt={result.title} src={result.image} />
+                      </figure>
+                    </div>
+                    <div className="column">
+                      <h5 className="bookTitle">
+                        {result.title} by {result.authors}
+                      </h5>
+                      <p className="bookDescriptionText">
+                        {result.description}
+                      </p>
+                    </div>
+                    <div className="column is-2">
+                      <div className="buttons">
+                        <button
+                          href={result.link}
+                          className="button is-primary"
+                          id="viewButton"
+                          target="_blank"
+                        >
+                          View
+                        </button>
+                        <button
+                          onClick={() => this.handleSave(result)}
+                          className="button is-primary"
+                          id="saveButton"
+                        >
+                          {this.state.savedBooks
+                            .map((book) => book.id)
+                            .includes(result._id)
+                            ? "Unsave"
+                            : "Save"}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </article>
+    );
+  }
 }
-}
-
 export default Results;
