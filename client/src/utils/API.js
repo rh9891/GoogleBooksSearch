f11a58b971bfe55ce750d22f10e6b9ede32e9729
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 07:54:47 -0400

    Completed the API file to make the axios calls to get a book, delete a book, save a book, and retrieve a saved book.

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    // Gets all books.
+    getBook: function (query) {
+        return axios.get("`https://www.googleapis.com/books/v1/volumes?q=${query}`")
+    },
+    // Deletes the book with a given id.
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id).then(result => result.data);
+    },
+    // Saves a book to the database
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+    // Gets a saved book from the database.
+    savedBooks: function() {
+        return axios.get("/api/books").then(result => result.data);
+    }
+};
