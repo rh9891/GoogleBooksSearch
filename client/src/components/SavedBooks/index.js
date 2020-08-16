2dcd0083fbd6625c3da90736ed0084c2372f36d4
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 00:31:09 -0400

    Added new IDs to the SavedBooks components, so that it is ready to target certain areas once the backend is up and running.

--- client/src/components/SavedBooks/index.js
+++ client/src/components/SavedBooks/index.js
@@ -3,7 +3,17 @@ import "./style.css";
 
 function SavedBooks() {
   return (
+      <>
      <br />
+     <article class="message is-primary">
+  <div class="message-header" id="savedBooksHeaderText">
+    <p>Saved Books</p>
+    <button class="delete" id="savedBooksDeleteButton" aria-label="delete"></button>
+  </div>
+  <div class="message-body" id="savedBooksResultsDiv">
+  </div>
+</article>
+</>
   );
 }
 
