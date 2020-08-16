8ea25835da7f20765477fc388f13aec4d99a8cfe
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 18:48:27 -0400

    Added the functions for the componentDidMount, makeBook (to render the book data from the API call), searchBook, handleInputChange, and handleFormSubmit.

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -4,10 +4,49 @@ import Results from "../Results";
 import API from "../../utils/API";
 
 class BookSearch extends Component() {
+  state = {
+    value: "",
+    books: []
+  };
+
+  componentDidMount() {
+    this.searchBook();
+  };
+
+  makeBook = bookData => {
+    return {
+      _id: bookData.id,
+      title: bookData.volumeInfo.title,
+      authors: bookData.volumeInfo.authors,
+      description: bookData.volumeInfo.description,
+      image: bookData.volumeInfo.imageLinks.thumbnail,
+      link: bookData.volumeInfo.previewLink
+    }
+  }
+
+  searchBook = query => {
+    API.getBook(query)
+      .then(res => this.setState({ books: res.data.items.map(bookData => this.makeBook(bookData)) }))
+      .catch(err => console.error(err));
+  };
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchBook(this.state.search);
+  }
+
   render() {
   return (
     <>
-    <Search 
+    <Search
     />
     <Results />
     </>
