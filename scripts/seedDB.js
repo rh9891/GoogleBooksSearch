a3705ad8d54f72a00d73d1693629b3ffe2a5a837
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 07:29:30 -0400

    Coded the logic so that the seeds could populate the application.

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -17,4 +17,16 @@ const bookSeed = [
         title: "The Hunger Games"
     }
 
-]
+];
+
+db.Book
+    .remove({})
+    .then(() => db.Book.collection.insertMany(bookSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted.");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err)
+        process.exit(1);
+    });
