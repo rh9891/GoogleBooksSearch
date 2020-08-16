e3e30fce6f369a54c373e0d5deef4911986ba7a4
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 13:27:34 -0400

    Added the componentDidMount and handleSave functions for the Results component.

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,7 +1,30 @@
 import React, {Component} from "react";
 import "./style.css";
+import API from "../../utils/API"
 
 class Results extends Component() {
+  state = {
+    savedBooks: [],
+  }
+
+  componentDidMount() {
+    API.savedBooks()
+      .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+      .catch(err => console.error(err));
+  }
+
+  handleSave = book => {
+    if (this.state.savedBooks.map(book => book._id).includes(book._id)) {
+      API.deleteBook(book._id)
+        .then(deletedBook => this.setState({ savedBooks: this.state.savedBooks.filter(book => book._id !== deleteBook._id) }))
+        .catch(err => console.error(err));
+    } else {
+      API.saveBook(book)
+        .then(savedBook => this.setState({ savedBooks: this.state.savedBooks.concat([savedBook])}))
+        .catch(err => console.error(err));
+    }
+  }
+
   render() {
   return (
 <>
