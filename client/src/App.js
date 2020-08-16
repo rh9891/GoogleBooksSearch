6a7ca3cbb74736aac3ebb35ea62d7b3c1622da60
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-15 20:05:09 -0400

    Imported the Results component to the App file in order to test the functionality of the component.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
 import Wrapper from "./components/Wrapper";
 import Search from "./components/Search";
+import Results from "./components/Results";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <NavBar />
     <Jumbotron />
     <Search />
+    <Results />
     </Wrapper>
   );
 }
